a380d857075c29a88b351e56b8a146ac957f2846
Matt Toigo <mtoigo@weddingwire.com>
2020-05-31 09:55:59 -0400

    Fixes to get deploy working

--- prod_deploy.sh
+++ prod_deploy.sh
@@ -1,1 +1,5 @@
+# Installed latest node through NVM
+# Run npm install
+# npm install -g @squarespace/server
+# Run ./node_modules/.bin/squarespace setup if you never have before
 ./node_modules/.bin/squarespace assemble && ./node_modules/.bin/webpack && ./node_modules/.bin/squarespace deploy
