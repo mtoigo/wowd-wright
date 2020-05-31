4d65354972c1f2774f265f3d94491ff4102ebcaa
Matt Toigo <mtoigo@weddingwire.com>
2020-05-31 10:24:29 -0400

    Cleanup up deployment command

--- prod_deploy.sh
+++ prod_deploy.sh
@@ -1,5 +1,8 @@
-# Installed latest node through NVM
+# PATH=$PATH:./node_modules/.bin (wasn't picking up executables in ./node_modules/.bin)
+# Installed latest node through NVM v14.3.0
 # Run npm install
 # npm install -g @squarespace/server
+# need to run squarespace setup
 # Run ./node_modules/.bin/squarespace setup if you never have before
-./node_modules/.bin/squarespace assemble && ./node_modules/.bin/webpack && ./node_modules/.bin/squarespace deploy
+#./node_modules/.bin/squarespace assemble && ./node_modules/.bin/webpack && ./node_modules/.bin/squarespace deploy
+# Use commands in package.json rather than this which is just here for cleanup/notes
